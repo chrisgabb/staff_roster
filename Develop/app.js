93af28f2e10e7a236f0535125d06313a914fba3a
bkny1020 <cgabb1020@gmail.com>
2020-09-26 22:25:54 -0400

    first

---
+++ Develop/app.js
@@ -0,0 +1,77 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+
+// Write code to use inquirer to gather information about the development team members,
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "position",
+        message: "Choose the position of the employee",
+        choices: ["Manager", "Engineer", "Intern"]
+      },
+      {
+        type: "input",
+        name: "name",
+        message: `Enter the name of the ${data.position} you are adding`,
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the  email address",
+      },
+      {
+        type: "input",
+        name: "officeid",
+        message: `Enter the ${data.postion}'s Office ID`,
+        when: (data) => data.position === "Manager"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: `Enter the ${data.position}'s Github URL`,
+        when: (data) => data.position === "Engineer"
+      },
+      {
+        type: "input",
+        name: "school",
+        message: `Enter the ${data.position}'s school name`,
+        when: (data) => data.position === "Intern"
+      },
+    ]);
+    
+  }
+
+
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
---
+++ Develop/lib/Employee.js
@@ -0,0 +1,5 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name)
+    this.name = name
+}
